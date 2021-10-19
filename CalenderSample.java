64a3b3bbae818eab45b02af0409e08aa66114960
ryota35 <ryota35.1025@gmail.com>
2021-10-19 23:05:12 +0900

    secound

--- CalenderSample.java
+++ CalenderSample.java
@@ -1,7 +1,22 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Calendar;
 
 public class CalenderSample {
   public static void main(String[] args) {
 
-    System.out.println("sssssssssssssss");
+    String[] week = { "", "日", "月", "火", "水", "木", "金", "土" };
+
+    Calendar cal = Calendar.getInstance();
+
+    cal.set(2021, 9, 19);
+
+    int i = cal.get(Calendar.DAY_OF_WEEK);
+
+    System.out.println(week[i]);
+
+    LocalDate ld = LocalDate.of(2021, 10, 19);
+    System.out.println(DayOfWeek.TUESDAY == ld.getDayOfWeek());
+
   }
 }
